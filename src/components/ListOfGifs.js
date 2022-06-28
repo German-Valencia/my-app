1645e8a312552dca194b4ccc494116f15469e915
Germán Alberto Valencia Torres <lenovopromotor@gmail.com>
2022-06-27 19:17:34 -0500

    1

---
+++ src/components/ListOfGifs.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import Gif from "./Gif";
+import getGifs from "../services/getGifs";
+
+export default function ListOfGifs({ params }) {
+  const { keyword } = params;
+
+  const [loading, setLoading] = useState(false);
+
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(
+    function () {
+      setLoading(true);
+      getGifs({ keyword }).then((gifs) => {
+        setGifs(gifs);
+        setLoading(false);
+      });
+    },
+    [keyword]
+  );
+
+  if (loading) return <i>Cargando</i>;
+
+  return (
+    <div>
+      {gifs.map(({ id, title, url }) => (
+        <Gif title={title} url={url} id={id} key={id} />
+      ))}
+    </div>
+  );
+}
